66b548d969ea138062457c75eba4c524ec549531
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-29 06:15:12 +0530

    Add all the files moved over from the original shell module

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+from shell.models.institute import (
+    Department,
+    Centre,
+    Branch,
+)
+from shell.models.personal_information import (
+    PoliticalInformation,
+)
+from shell.models.roles import (
+    Student,
+    FacultyMember,
+)
