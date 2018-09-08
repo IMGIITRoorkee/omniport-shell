6f0ef7d84d21bbc413b5f5918c9f7effd5174b81
Shaddy Garg <shaddygarg1@gmail.com>
2018-09-09 02:44:10 +0530

    Complete function which grants different rights to maintainers based on their current semester number

--- utils/rights.py
+++ utils/rights.py
@@ -1,3 +1,34 @@
+from omniport.middleware.person_roles import get_role
+from kernel.mixins.period_mixin import ActiveStatus
+
+
+def get_rights_for_semester(user, semester_no):
+    """
+    Check if the given user has enough privileges based on semester number
+    :param user: the user whose privileges are being tested
+    :param semester_no: the minimum semester number in which rights are given
+    :return: True if the user has privileges, False otherwise
+    """
+
+    student = get_role(
+        person=user.person,
+        role_name='Student',
+        active_status=ActiveStatus.IS_ACTIVE,
+        silent=True
+    )
+
+    maintainer = get_role(
+        person=user.person,
+        role_name='Maintainer',
+        active_status=ActiveStatus.IS_ACTIVE,
+        silent=True
+    )
+
+    if maintainer is not None:
+        return student.current_semester > semester_no
+    return False
+
+
 def has_omnipotence_rights(user):
     """
     Check if the given user has enough privileges to access the admin interface
@@ -5,7 +36,7 @@ def has_omnipotence_rights(user):
     :return: True if the user has privileges, False otherwise
     """
 
-    return True  # TODO: Find a proper way to determine privileges
+    return get_rights_for_semester(semester_no=5, user=user)
 
 
 def has_alohomora_rights(user):
@@ -14,8 +45,7 @@ def has_alohomora_rights(user):
     :param user: the user whose privileges are being tested
     :return: True if the user has privileges, False otherwise
     """
-
-    return True  # TODO: Find a proper way to determine privileges
+    return get_rights_for_semester(semester_no=5, user=user)
 
 
 def has_lockpicking_rights(user):
@@ -25,4 +55,14 @@ def has_lockpicking_rights(user):
     :return: True if the user has privileges, False otherwise
     """
 
-    return True  # TODO: Find a proper way to determine privileges
+    return get_rights_for_semester(semester_no=4, user=user)
+
+
+def has_helpcentre_rights(user):
+    """
+    Check if the given user has enough privileges to access helpcentre
+    :param user: the user whose privileges are being tested
+    :return: True if the user has privileges, False otherwise
+    """
+
+    return get_rights_for_semester(semester_no=4, user=user)
