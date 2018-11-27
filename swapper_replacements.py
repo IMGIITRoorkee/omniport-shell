dd7588e623b14275c1dba72fafbb88b2f1740391
Dhruv Bhanushali <dhruv_b@live.com>
2018-11-28 02:52:01 +0530

    Rename the file `swapper.py` to `swapper_replacement.py` for non-conflict with the package swapper also in the PYTHONPATH

--- swapper_replacements.py
+++ swapper_replacements.py
@@ -1,7 +1,7 @@
 KERNEL_POLITICALINFORMATION_MODEL = 'shell.PoliticalInformation'
 KERNEL_DEPARTMENT_MODEL = 'shell.Department'
 KERNEL_CENTRE_MODEL = 'shell.Centre'
-KERNEL_BRANCH_MODEL = 'shell.Branch'
+KERNEL_DEGREE_MODEL = 'shell.Degree'
 KERNEL_STUDENT_MODEL = 'shell.Student'
 KERNEL_FACULTYMEMBER_MODEL = 'shell.FacultyMember'
 KERNEL_MAINTAINER_MODEL = 'shell.Maintainer'
