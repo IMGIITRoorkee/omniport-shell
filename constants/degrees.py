1432cf6c33e9970c131bc0ded514c751dd3105d6
Dhruv Bhanushali <dhruv_b@live.com>
2018-11-28 02:46:14 +0530

    Fix degree constants which were written completely wrong

--- constants/degrees.py
+++ constants/degrees.py
@@ -2,52 +2,95 @@
 Degrees offered by IIT Roorkee
 """
 
-BACHELOR_OF_TECHNOLOGY = 'B.Tech. - Bachelor of Technology'
-MASTER_OF_TECHNOLOGY = 'M.Tech. - Master of Technology'
-INTEGRATED_DUAL_DEGREE = 'Int. M.Tech. - Integrated Dual Degree'
-
-BACHELOR_OF_SCIENCE = 'B.Sc. - Bachelor of Science'
-MASTER_OF_SCIENCES = 'M.Sc. - Master of Science'
-INTEGRATED_MASTER_OF_SCIENCE = 'Int. M.Sc. - Integrated Master of Science'
-
-BACHELOR_OF_ARCHITECTURE = 'B.Arch. - Bachelor of Architecture'
-MASTER_OF_ARCHITECTURE = 'M.Arch. - Master of Architecture'
-MASTER_OF_URBAN_AND_REGIONAL_PLANNING = ('M.U.R.P - Master of Urban and '
-                                         'Regional Planning')
-
-DOCTOR_OF_PHILOSOPHY = 'Ph.D. - Doctor of Philosophy'
-POST_DOCTORATE = 'Post Doc. - Post-doctorate'
-
-MASTER_OF_BUSINESS_ADMINISTRATION = 'M.B.A. - Master of Business Administration'
-MASTER_OF_COMPUTER_APPLICATIONS = 'M.C.A. - Master of Computer Applications'
-
-POSTGRADUATE_DIPLOMA_COURSE = 'P.G. Dip. - Post-graduate Diploma'
-
-UNDERGRADUATE_DEGREES = (
-    ('btech', BACHELOR_OF_TECHNOLOGY),
-    ('idd', INTEGRATED_DUAL_DEGREE),
-    ('bsc', BACHELOR_OF_SCIENCE),
-    ('imsc', INTEGRATED_MASTER_OF_SCIENCE),
-    ('barch', BACHELOR_OF_ARCHITECTURE),
+# Graduate degrees
+BACHELOR_OF_TECHNOLOGY = 'btech'
+INTEGRATED_DUAL_DEGREE = 'idd'
+BACHELOR_OF_SCIENCE = 'bsc'
+BACHELOR_OF_ARCHITECTURE = 'imsc'
+INTEGRATED_MASTER_OF_SCIENCE = 'barch'
+GRADUATE_DEGREES = (
+    (
+        BACHELOR_OF_TECHNOLOGY,
+        'B.Tech. - Bachelor of Technology',
+    ),
+    (
+        INTEGRATED_DUAL_DEGREE,
+        'Int. M.Tech. - Integrated Dual Degree',
+    ),
+    (
+        BACHELOR_OF_SCIENCE,
+        'B.Sc. - Bachelor of Science',
+    ),
+    (
+        BACHELOR_OF_ARCHITECTURE,
+        'B.Arch. - Bachelor of Architecture',
+    ),
+    (
+        INTEGRATED_MASTER_OF_SCIENCE,
+        'Int. M.Sc. - Integrated Master of Science',
+    ),
 )
 
+# Postgraduate degrees
+MASTER_OF_TECHNOLOGY = 'mtech'
+MASTER_OF_SCIENCES = 'msc'
+MASTER_OF_ARCHITECTURE = 'march'
+MASTER_OF_URBAN_AND_REGIONAL_PLANNING = 'murp'
+POSTGRADUATE_DIPLOMA_COURSE = 'pgdip'
+MASTER_OF_BUSINESS_ADMINISTRATION = 'mba'
+MASTER_OF_COMPUTER_APPLICATIONS = 'mca'
 POSTGRADUATE_DEGREES = (
-    ('mtech', MASTER_OF_TECHNOLOGY),
-    ('msc', MASTER_OF_SCIENCES),
-    ('march', MASTER_OF_ARCHITECTURE),
-    ('murp', MASTER_OF_URBAN_AND_REGIONAL_PLANNING),
-    ('pgdip', POSTGRADUATE_DIPLOMA_COURSE),
-    ('mba', MASTER_OF_BUSINESS_ADMINISTRATION),
-    ('mca', MASTER_OF_COMPUTER_APPLICATIONS),
+    (
+        MASTER_OF_TECHNOLOGY,
+        'M.Tech. - Master of Technology',
+    ),
+    (
+        MASTER_OF_SCIENCES,
+        'M.Sc. - Master of Science',
+    ),
+    (
+        MASTER_OF_ARCHITECTURE,
+        'M.Arch. - Master of Architecture',
+    ),
+    (
+        MASTER_OF_URBAN_AND_REGIONAL_PLANNING,
+        'M.U.R.P - Master of Urban and Regional Planning',
+    ),
+    (
+        POSTGRADUATE_DIPLOMA_COURSE,
+        'P.G. Dip. - Post-graduate Diploma',
+    ),
+    (
+        MASTER_OF_BUSINESS_ADMINISTRATION,
+        'M.B.A. - Master of Business Administration',
+    ),
+    (
+        MASTER_OF_COMPUTER_APPLICATIONS,
+        'M.C.A. - Master of Computer Applications',
+    ),
 )
 
+# Doctorate degrees
+DOCTOR_OF_PHILOSOPHY = 'phd'
 DOCTORATE_DEGREES = (
-    ('phd', DOCTOR_OF_PHILOSOPHY),
-    ('pdoc', POST_DOCTORATE),
+    (
+        DOCTOR_OF_PHILOSOPHY,
+        'Ph.D. - Doctor of Philosophy',
+    ),
+)
+
+# Postdoctorate degrees
+POSTDOCTORATE = 'pdoc'
+POSTDOCTORATE_DEGREES = (
+    (
+        POSTDOCTORATE,
+        'Post Doc. - Post-doctorate',
+    ),
 )
 
 DEGREES = (
-        UNDERGRADUATE_DEGREES
+        GRADUATE_DEGREES
         + POSTGRADUATE_DEGREES
         + DOCTORATE_DEGREES
+        + POSTDOCTORATE_DEGREES
 )
