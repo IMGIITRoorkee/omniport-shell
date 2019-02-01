3d324b6484c2bb802c601e2ea82970586f909c3c
Dhruv Bhanushali <dhruv_b@live.com>
2019-02-02 01:21:59 +0530

    Override PersonalInformationSerializer defined in the shell

---
+++ serializers/personal_information/political_information.py
@@ -0,0 +1,47 @@
+import swapper
+
+from kernel.serializers.personal_information.political_information import (
+    PoliticalInformationSerializer as BasePoliticalInformationSerializer
+)
+
+PoliticalInformation = swapper.load_model('kernel', 'PoliticalInformation')
+
+
+class PoliticalInformationSerializer(BasePoliticalInformationSerializer):
+    """
+    Serializer for PoliticalInformation objects
+    """
+
+    # See superclass
+    editable_fields = [
+        'religion',
+        'passport_number',
+        'driving_license_number',
+    ]
+
+    class Meta:
+        """
+        Meta class for PoliticalInformationSerializer
+        """
+
+        model = PoliticalInformation
+        exclude = [
+            'person',
+            'id',
+            'datetime_created',
+            'datetime_modified',
+        ]
+
+    def to_representation(self, instance):
+        """
+        Add reservation category to the representation
+        :param instance: the instance being represented
+        :return: the dictionary representation of the instance
+        """
+
+        representation = super().to_representation(instance)
+
+        reservation_category = instance.get_reservation_category_display()
+        representation['reservation_category'] = reservation_category
+
+        return representation
---
+++ serializers/registration.py
@@ -0,0 +1,8 @@
+KERNEL_POLITICALINFORMATION_SERIALIZER = (
+    'shell.serializers.personal_information.'
+    'political_information.PoliticalInformationSerializer'
+)
+
+__all__ = [
+    'KERNEL_POLITICALINFORMATION_SERIALIZER',
+]
